f5d9d6c3818df3cffeb14b5480bfc9c29521d850
oussem76 <oussem93@hotmail.fr>
2016-10-03 13:42:44 +0200

    Ajout de la classe Cone et son test

--- src/Cone.java
+++ src/Cone.java
@@ -11,18 +11,35 @@ public class Cone extends Shape3D {
         this.radius = radius;
     }
 
+
+
     public Cone() {
         this(0,0);
     }
 
-
+    @Override
     protected double volume(){
-
         return ((Math.PI*this.radius*this.radius*this.height)/3);
     }
 
     @Override
     double surface() {
         return ((Math.PI*this.radius)*Math.sqrt(this.height*this.height + this.radius*this.radius));
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
     }
 }
--- test/ConeTest.java
+++ test/ConeTest.java
@@ -8,6 +8,8 @@ import static org.testng.Assert.*;
  * Created by OOussema on 03/10/2016.
  */
 public class ConeTest {
+
+
     @BeforeMethod
     public void setUp() throws Exception {
 
@@ -17,15 +19,36 @@ public class ConeTest {
     public void tearDown() throws Exception {
 
     }
-
     @Test
-    public void testVolume() throws Exception {
+    public void testGetHeight() throws Exception {
+
+    }
+
+    @Test(enabled = false)
+    public void testSetHeight() throws Exception {
+
+    }
+
+    @Test(enabled = false)
+    public void testGetRadius() throws Exception {
+
+    }
+
+    @Test(enabled = false)
+    public void testSetRadius() throws Exception {
 
     }
 
     @Test
-    public void testSurface() throws Exception {
+    public void testVolume() throws Exception {
+        Cone c = new Cone(5,5);
+        assertEquals(130.9, c.volume(), 0.3);
+    }
 
+    @Test
+    public void testSurface() throws Exception {
+        Cone c = new Cone(5,5);
+        assertEquals(111.07207, c.surface(), 0.3);
     }
 
 }
